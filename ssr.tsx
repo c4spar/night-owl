6b18a00cf492bb4ede97a91c45373b17b42805fd
Benjamin Fischer <c4spar@gmx.de>
2021-12-06 19:39:43 +0100

    refactor: remove title from index html

--- ssr.tsx
+++ ssr.tsx
@@ -19,7 +19,6 @@ function Html({ body, head, footer }: AppOptions) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Cliffy</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" />
         ${head.join("\n")}
       </head>
