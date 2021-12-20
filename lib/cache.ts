c1ce1939f1e6e8e88eca06a3e2a029c829b96cb0
Benjamin Fischer <c4spar@gmx.de>
2021-12-20 01:53:20 +0100

    refactor: make Cache.isEnabled() method static

--- lib/cache.ts
+++ lib/cache.ts
@@ -2,14 +2,14 @@ export class Cache<T> {
   #cache = new Map<string, T>();
 
   get(key: string): T | undefined {
-    return this.isEnabled() ? this.#cache.get(key) : undefined;
+    return Cache.isEnabled() ? this.#cache.get(key) : undefined;
   }
 
   set(key: string, value: T): void {
     this.#cache.set(key, value);
   }
 
-  isEnabled(): boolean {
+  static isEnabled(): boolean {
     return Deno.env.get("NO_CACHE")?.toLowerCase() !== "true";
   }
 }
