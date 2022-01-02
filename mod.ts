c1b2141d4b5298db74641f9b2fb5b3be4147e3ef
Benjamin Fischer <c4spar@gmx.de>
2022-01-02 18:35:52 +0100

    refactor: add exports to mod.ts

--- mod.ts
+++ mod.ts
@@ -1,5 +1,43 @@
 import { setupLog } from "./lib/log.ts";
 
-export { serve } from "./lib/serve.tsx";
-
 await setupLog();
+
+/** COMPONENTS */
+export { NotFound, type NotFoundOptions } from "./components/not_found.tsx";
+export { AnimatedText } from "./components/animated_text.tsx";
+export {
+  type ButtonOptions,
+  PrimaryButton,
+  SecondaryButton,
+} from "./components/buttons.tsx";
+export { Code } from "./components/code.tsx";
+
+/** LIB */
+export {
+  type ComponentType,
+  Page,
+  type PageDecoratorOptions,
+} from "./lib/page.ts";
+export { serve } from "./lib/serve.tsx";
+export {
+  type AppConfig,
+  type CreateConfigOptions,
+  type NavOptions,
+} from "./lib/config.ts";
+export {
+  type Provider,
+  type ProviderFunction,
+  type ProviderType,
+} from "./lib/provider.ts";
+export {
+  type FileOptions,
+  getFiles,
+  type GetFilesOptions,
+  type ReadDirOptions,
+} from "./lib/resource.ts";
+export { bgMain, textMain, transformGpu } from "./lib/styles.ts";
+export { capitalize, sortByKey } from "./lib/utils.ts";
+export { type ChildComponent, type Children } from "./lib/types.ts";
+
+/** DEPS */
+export { Component, Fragment, h, Helmet, render, tw, apply, css, theme, twColors } from "./deps.ts";
