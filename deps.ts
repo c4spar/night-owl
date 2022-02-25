fc8284235a9437da6c7b865707cf50a30c6caa7f
Benjamin Fischer <c4spar@gmx.de>
2022-02-25 01:39:03 +0100

    chore(upgrade): upgrade deps & deno/std to v0.121.0

--- deps.ts
+++ deps.ts
@@ -1,12 +1,12 @@
 /** std */
-export { serve } from "https://deno.land/std@0.121.0/http/server.ts";
+export { serve } from "https://deno.land/std@0.127.0/http/server.ts";
 export {
   blue,
   bold,
   green,
   red,
   yellow,
-} from "https://deno.land/std@0.121.0/fmt/colors.ts";
+} from "https://deno.land/std@0.127.0/fmt/colors.ts";
 export {
   basename,
   dirname,
@@ -15,37 +15,36 @@ export {
   isAbsolute,
   join,
   resolve,
-} from "https://deno.land/std@0.121.0/path/mod.ts";
-export * as log from "https://deno.land/std@0.121.0/log/mod.ts";
+} from "https://deno.land/std@0.127.0/path/mod.ts";
+export * as log from "https://deno.land/std@0.127.0/log/mod.ts";
 export {
   decode as decodeBase64,
   encode as encodeBase64,
-} from "https://deno.land/std@0.121.0/encoding/base64.ts";
-export { assert } from "https://deno.land/std@0.121.0/testing/asserts.ts";
-export { debounce } from "https://deno.land/std@0.121.0/async/debounce.ts";
-export { deepMerge } from "https://deno.land/std@0.121.0/collections/deep_merge.ts";
-export { parse as parseYaml } from "https://deno.land/std@0.121.0/encoding/yaml.ts";
+} from "https://deno.land/std@0.127.0/encoding/base64.ts";
+export { assert } from "https://deno.land/std@0.127.0/testing/asserts.ts";
+export { debounce } from "https://deno.land/std@0.127.0/async/debounce.ts";
+export { deepMerge } from "https://deno.land/std@0.127.0/collections/deep_merge.ts";
+export { parse as parseYaml } from "https://deno.land/std@0.127.0/encoding/yaml.ts";
 
 /** media_types */
 export {
   contentType,
   lookup,
-} from "https://deno.land/x/media_types@v2.11.1/mod.ts";
+} from "https://deno.land/x/media_types@v2.12.2/mod.ts";
 
 /** nano_jsx */
-export { Helmet } from "https://deno.land/x/nano_jsx@v0.0.26/components/helmet.ts";
-export { h, render } from "https://deno.land/x/nano_jsx@v0.0.26/core.ts";
-export { Fragment } from "https://deno.land/x/nano_jsx@v0.0.26/fragment.ts";
-export { Component } from "https://deno.land/x/nano_jsx@v0.0.26/component.ts";
-export { renderSSR } from "https://deno.land/x/nano_jsx@v0.0.26/ssr.ts";
-export { Store } from "https://deno.land/x/nano_jsx@v0.0.26/store.ts";
+export { Helmet } from "https://deno.land/x/nano_jsx@v0.0.29/components/helmet.ts";
+export { h, render } from "https://deno.land/x/nano_jsx@v0.0.29/core.ts";
+export { Fragment } from "https://deno.land/x/nano_jsx@v0.0.29/fragment.ts";
+export { Component } from "https://deno.land/x/nano_jsx@v0.0.29/component.ts";
+export { renderSSR } from "https://deno.land/x/nano_jsx@v0.0.29/ssr.ts";
+export { Store } from "https://deno.land/x/nano_jsx@v0.0.29/store.ts";
 export {
   getState,
   setState,
-} from "https://deno.land/x/nano_jsx@v0.0.26/hooks/useState.ts";
+} from "https://deno.land/x/nano_jsx@v0.0.29/hooks/useState.ts";
 
 /** twind */
-// @deno-types=https://cdn.skypack.dev/-/twind@v0.16.16-LPGqCzM3XVHFUO0IDjyk/dist=es2020,mode=types/twind.d.ts
 export {
   apply,
   type CSSRules,
@@ -53,34 +52,26 @@ export {
   setup,
   strict,
   type Theme,
-  tw,
-} from "https://cdn.skypack.dev/-/twind@v0.16.16-LPGqCzM3XVHFUO0IDjyk/dist=es2020,mode=imports/optimized/twind.js";
-
-// @deno-types=https://cdn.skypack.dev/-/twind@v0.16.16-LPGqCzM3XVHFUO0IDjyk/dist=es2020,mode=types/css/css.d.ts
-export {
-  css,
   theme,
-} from "https://cdn.skypack.dev/-/twind@v0.16.16-LPGqCzM3XVHFUO0IDjyk/dist=es2020,mode=imports/optimized/twind/css.js";
-
-// @deno-types=https://cdn.skypack.dev/-/twind@v0.16.16-LPGqCzM3XVHFUO0IDjyk/dist=es2020,mode=types/sheets/sheets.d.ts
+  tw,
+} from "https://esm.sh/twind@0.16.16";
+export { css } from "https://esm.sh/twind@0.16.16/css";
 export {
   getStyleTag,
   virtualSheet,
-} from "https://cdn.skypack.dev/-/twind@v0.16.16-LPGqCzM3XVHFUO0IDjyk/dist=es2020,mode=imports/optimized/twind/sheets.js";
-
-// @deno-types=https://cdn.skypack.dev/-/twind@v0.16.16-LPGqCzM3XVHFUO0IDjyk/dist=es2020,mode=types/colors/colors.d.ts
-export * as twColors from "https://cdn.skypack.dev/-/twind@v0.16.16-LPGqCzM3XVHFUO0IDjyk/dist=es2020,mode=imports/optimized/twind/colors.js";
+} from "https://esm.sh/twind@0.16.16/shim/server";
+export * as twColors from "https://esm.sh/twind@0.16.16/colors";
 
 /** comrak */
 import * as comrak from "https://deno.land/x/comrak@0.1.1/mod.ts";
-
 await comrak.init();
 export { comrak };
 
-export { lowlight } from "https://esm.sh/lowlight@2.4.1?pin=v58";
+/** lowlight */
+export { lowlight } from "https://esm.sh/lowlight@2.5.0";
 
-// Used to sanitize some output, ensuring html entities are encoded.
-export * as htmlEntities from "https://cdn.skypack.dev/html-entities@2.3.2?dts";
+/** html-entities */
+export * as htmlEntities from "https://esm.sh/html-entities@2.3.2";
 
-// Used to convert lowlight trees to HTML
-export { toHtml } from "https://esm.sh/hast-util-to-html@8.0.3?pin=v58";
+/** hast-util-to-html */
+export { toHtml } from "https://esm.sh/hast-util-to-html@8.0.3";
