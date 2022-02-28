bc7f5eebd588e37843e8118ff8abab1f794ba179
Benjamin Fischer <c4spar@gmx.de>
2022-02-28 21:42:59 +0100

    docs: fix example

--- examples/cliffy_manual.ts
+++ examples/cliffy_manual.ts
@@ -2,6 +2,6 @@ import { serve } from "../mod.ts";
 
 await serve({
   repository: "c4spar/deno-cliffy",
-  src: [{ src: "c4spar/cliffy-docs@main:src" }],
+  src: [{ src: "c4spar/cliffy-manual@main:/" }],
   nav: { collapse: true },
 });
