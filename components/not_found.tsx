e3026122a67d65bfc25f4c3dfb64bdd13b8d151f
Benjamin Fischer <c4spar@gmx.de>
2022-03-08 00:12:34 +0100

    refactor: update NotFound component

--- components/not_found.tsx
+++ components/not_found.tsx
@@ -1,6 +1,7 @@
 /** @jsx h */
 
 import { Fragment, h, log, tw } from "../deps.ts";
+import { styles } from "../lib/styles.ts";
 import { AnimatedText } from "./animated_text.tsx";
 
 export interface NotFoundOptions {
@@ -15,8 +16,12 @@ export function NotFound({ url }: NotFoundOptions) {
         speed={6}
         class={tw`container mx-auto p-5 mt-[10%] font-nerd text-xl text-center`}
       >
-        Oops, you have requested a site that does not exist!
+        Oops, you have requested a site that doesn't exist!
       </AnimatedText>
+      <div class={tw`pl-5 pr-5 mt-[5rem]
+        font-mija-bold text([10rem] md:[15rem] lg:[25rem]) text-center opacity-10
+        ${styles.text.secondary} ${styles.transform.primary}`}>404
+      </div>
     </Fragment>
   );
 }
